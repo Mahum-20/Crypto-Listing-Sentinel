ddb9972500f31ccebe50428a82037fcfee320c7e
mahum-20 <nmahum216@gmail.com>
2026-08-28 19:35:22 +0500

    Add redirection route for favicon.ico

--- NewCoinSetup/urls.py
+++ NewCoinSetup/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon.svg')),
     path('', include('NewCoinSetupApp.urls')),
 ]
